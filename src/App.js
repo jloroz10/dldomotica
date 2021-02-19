065c049c82617cda9e05772b48693a7f8ebfcb82
Jose Luis <jose.j.orozco@oracle.com>
2021-02-18 22:39:32 -0600

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 import underConstruction from "./img/under_construction.png";
+import logo from "./img/dldomotica_portada.JPG";
 function App() {
   return (
     <div className="App">
-        <h1>dldomotica</h1>
+        <div>
+			<img className="logo" alt="logo" src={logo}></img>
+		</div>
        <div>
           <img className="construction" alt="under construction" src={underConstruction}></img>
         </div>
