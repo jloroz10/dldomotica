b6a482b35d8982999976b40f90a5ea7e7354a6df
Jose Luis <jose.j.orozco@oracle.com>
2021-02-18 22:11:06 -0600

    fist commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import underConstruction from "./img/under_construction.png";
+function App() {
+  return (
+    <div className="App">
+     <h1>"DLDOMOTICA"</h1>
+     <img className="construction" alt="under construction" src={underConstruction}></img>
+    </div>
+  );
+}
+
+export default App;
