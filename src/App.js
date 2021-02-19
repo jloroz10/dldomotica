fd304c35d36353e70a5a6a834552360de56820f6
Jose Luis <jose.j.orozco@oracle.com>
2021-02-18 22:31:22 -0600

    centering elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+
 import underConstruction from "./img/under_construction.png";
 function App() {
   return (
     <div className="App">
-     <h1>dldomotica</h1>
-     <img className="construction" alt="under construction" src={underConstruction}></img>
+        <h1>dldomotica</h1>
+       <div>
+          <img className="construction" alt="under construction" src={underConstruction}></img>
+        </div>
     </div>
   );
 }
